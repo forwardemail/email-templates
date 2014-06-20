1219d9b6421311fb0ca81b84c3304fd5a4731d27
Ryan Wheale <ryan.wheale@gmail.com>
2014-06-20 17:11:28 -0600

    Updated glob pattern to allow filenames with custom (arbitrary) prefixes.  All tests passing

--- test/integration.js
+++ test/integration.js
@@ -35,12 +35,41 @@ describe('Email templates', function() {
         })
       })
     })
+    
+    it('html file with custom name', function(done) {
+      var html = '<h4><%= item%></h4>'
+      fs.writeFileSync(path.join(templateDir, templateName, 'custom-filename-html.ejs'), html)
+      emailTemplates(templateDir, function(err, template) {
+        template(templateName, {item: 'test'}, function(err, html, text) {
+          expect(err).to.be.null
+          expect(text).to.be.false
+          expect(html).to.equal('<h4>test</h4>')
+          done()
+        })
+      })
+    })
 
     it('html and text files', function(done) {
       var html = '<h4><%= item%></h4>'
         , text = '<%= item%>'
       fs.writeFileSync(path.join(templateDir, templateName, 'html.ejs'), html)
       fs.writeFileSync(path.join(templateDir, templateName, 'text.ejs'), text)
+
+      emailTemplates(templateDir, function(err, template) {
+        template(templateName, {item: 'test'}, function(err, html, text) {
+          expect(err).to.be.null
+          expect(html).to.equal('<h4>test</h4>')
+          expect(text).to.equal('test')
+          done()
+        })
+      })
+    })
+    
+    it('html and text files with custom names', function(done) {
+      var html = '<h4><%= item%></h4>'
+        , text = '<%= item%>'
+      fs.writeFileSync(path.join(templateDir, templateName, 'custom-filename-html.ejs'), html)
+      fs.writeFileSync(path.join(templateDir, templateName, 'custom-filename-text.ejs'), text)
 
       emailTemplates(templateDir, function(err, template) {
         template(templateName, {item: 'test'}, function(err, html, text) {
@@ -59,6 +88,25 @@ describe('Email templates', function() {
       fs.writeFileSync(path.join(templateDir, templateName, 'html.ejs'), html)
       fs.writeFileSync(path.join(templateDir, templateName, 'text.ejs'), text)
       fs.writeFileSync(path.join(templateDir, templateName, 'style.ejs'), css)
+
+      emailTemplates(templateDir, function(err, template) {
+        template(templateName, {item: 'test'}, function(err, html, text) {
+          expect(err).to.be.null
+          expect(text).to.equal('test')
+          expect(html).to.equal(
+            '<html><body><h4 style=\"color: #ccc;\">test</h4></body></html>')
+          done()
+        })
+      })
+    })
+    
+	it('html with inline CSS and text file with custom names', function(done) {
+      var html = '<h4><%= item%></h4>'
+        , text = '<%= item%>'
+        , css  = 'h4 { color: #ccc }'
+      fs.writeFileSync(path.join(templateDir, templateName, 'custom-filename-html.ejs'), html)
+      fs.writeFileSync(path.join(templateDir, templateName, 'custom-filename-text.ejs'), text)
+      fs.writeFileSync(path.join(templateDir, templateName, 'custom-filename-style.ejs'), css)
 
       emailTemplates(templateDir, function(err, template) {
         template(templateName, {item: 'test'}, function(err, html, text) {
@@ -126,6 +174,18 @@ describe('Email templates', function() {
           expect(text).to.be.undefined
           expect(err.code).to.equal('ENOENT')
 
+          done()
+        })
+      })
+    })
+
+    it('on misnamed html file', function(done) {
+      fs.writeFileSync(path.join(templateDir, templateName, 'html-custom-filename.ejs'), '')
+      emailTemplates(templateDir, function(err, template) {
+        template(templateName, {item: 'test'}, function(err, html, text) {
+          expect(html).to.be.undefined
+          expect(text).to.be.undefined
+          expect(err.code).to.equal('ENOENT')
           done()
         })
       })
