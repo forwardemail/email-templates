60c46cc3b505108b073ad0d8b544022de4779cb7
niftylettuce <niftylettuce@gmail.com>
2019-01-09 17:38:17 -0600

    docs: fixed typos and devDeps

---
+++ examples/sendgrid.js
@@ -0,0 +1,31 @@
+const Email = require('..');
+const nodemailer = require('nodemailer');
+const nodemailerSendgrid = require('nodemailer-sendgrid');
+
+const transport = nodemailer.createTransport(
+  nodemailerSendgrid({
+    apiKey: 'your API KEY HERE'
+  })
+);
+
+const email = new Email({
+  message: {
+    from: 'niftylettuce@gmail.com'
+  },
+  // uncomment below to send emails in development/test env:
+  // send: true
+  transport
+});
+
+email
+  .send({
+    template: 'mars',
+    message: {
+      to: 'elon@spacex.com'
+    },
+    locals: {
+      name: 'Elon'
+    }
+  })
+  .then(console.log)
+  .catch(console.error);
